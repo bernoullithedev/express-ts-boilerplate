35bcfff5ab0e454c9f39f4811651d5c874ee090e
Arnav Sharma <arnavsharma2711@gmail.com>
2025-01-12 19:40:09 +0530

    initialize express TypeScript boilerplate with basic configuration and health check endpoint

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import morganLogger from "morgan";
+import helmetSecurity from "helmet";
+import corsMiddleware from "cors";
+import cookieParserMiddleware from "cookie-parser";
+import path from "node:path";
+import responseMiddleware from "./middlewares/response";
+
+import { handleError, handleNotFound } from "./middlewares/middlewares";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const expressApp = express();
+expressApp.use(morganLogger("dev"));
+expressApp.use(helmetSecurity());
+expressApp.use(corsMiddleware());
+expressApp.use(express.json());
+expressApp.use(cookieParserMiddleware());
+expressApp.use(responseMiddleware);
+
+// Define root route
+expressApp.get("/", (_req, res) => {
+	res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+// Importing API routes
+import apiRoutes from "./routes";
+expressApp.use("/api", apiRoutes);
+
+// Use custom middlewares for handling 404 and errors
+expressApp.use(handleNotFound);
+expressApp.use(handleError);
+
+export default expressApp;
